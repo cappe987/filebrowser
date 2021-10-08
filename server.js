8b862ebd4d77dcab5957fec02b2bd6ed9033d2a9
Tarek Ali <tarekali15@outlook.com>
2021-10-08 09:22:26 -0700

    proper formatting for JS

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const fs      = require("fs"     );
-const path    = require("path"   );
-const util    = require("util"   );
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
 
-const statAsync =     util.promisify(fs.stat    );
+const statAsync = util.promisify(fs.stat);
 const readFileAsync = util.promisify(fs.readFile);
-const readDirAsync  = util.promisify(fs.readdir );
+const readDirAsync = util.promisify(fs.readdir);
 
 const app = express();
 const port = 3000;
@@ -14,27 +14,27 @@ const rootdir = __dirname;
 // const rootdir = "/home/casper/Pictures";
 
 app.use(express.static("./client/"));
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
 
-function getExtension(absolutepath){
+function getExtension(absolutepath) {
   return path.extname(absolutepath).substring(1);
 }
 
-function getFiletype(stat, relativepath){
-  if (stat.isFile()){
+function getFiletype(stat, relativepath) {
+  if (stat.isFile()) {
     const img = ["png", "jpg", "jpeg", "bmp", "gif", "apng", "svg", "ico"];
     const ext = getExtension(relativepath);
     return img.includes(ext) ? "image" : "textfile";
   }
-  else if (stat.isDirectory()){
+  else if (stat.isDirectory()) {
     return "directory";
   }
   return "invalid";
 }
 
-function getAbsolutepath(relativepath){
+function getAbsolutepath(relativepath) {
   const absolutepath = path.join(rootdir, relativepath);
   return decodeURI(absolutepath); //To handle spaces in path
 }
@@ -44,21 +44,21 @@ app.post('/open', async (req, res) => {
   const relativepath = path.normalize(req.body.relativepath);
   const absolutepath = getAbsolutepath(relativepath);
 
-  try{
+  try {
     const stat = await statAsync(absolutepath);
-    switch (getFiletype(stat, relativepath)){
-      case "textfile" :
+    switch (getFiletype(stat, relativepath)) {
+      case "textfile":
         const contents = await readFileAsync(absolutepath, "utf8");
-        res.json({type: "textfile", newdir: relativepath, data: contents});
+        res.json({ type: "textfile", newdir: relativepath, data: contents });
         break;
 
-      case "directory": 
+      case "directory":
         const files = await readDirAsync(absolutepath);
-        res.json({type: "directory", newdir: relativepath, data: files});
+        res.json({ type: "directory", newdir: relativepath, data: files });
         break;
-      
+
       case "image":
-        res.json({type: "image", newdir: relativepath});
+        res.json({ type: "image", newdir: relativepath });
         break;
 
       default:
@@ -66,8 +66,8 @@ app.post('/open', async (req, res) => {
         return;
     }
   }
-  catch(e){ 
-    console.log("Caught Error: " + e); 
+  catch (e) {
+    console.log("Caught Error: " + e);
     return;
   }
 });
@@ -79,12 +79,5 @@ app.get('/image/*', (req, res) => {
   const absolutepath = getAbsolutepath(relativepath);
   res.sendFile(absolutepath);
 });
-
-
-
-
-
-
-
 
 // Move client-code to a lower folder.
